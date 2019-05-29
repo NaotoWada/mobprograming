5ad5874dceca977e4d9c6e5d8d1a4c4578bd9e71
NaotoWada <naoto.cot25@gmail.com>
2019-05-29 23:03:59 +0900

    upgrade:明白な実装によって最初の文字がクローズだった場合のメソッド修正

--- src/mobpro/own/BalancedParentheses.java
+++ src/mobpro/own/BalancedParentheses.java
@@ -1,6 +1,11 @@
 package mobpro.own;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class BalancedParentheses {
+
+	private static final List<String> CLOSE_LIST = Arrays.asList("]", ")", "}");
 
 	public static boolean isOdd(String parentheses) {
 		if(parentheses.length() % 2 ==0) {
@@ -10,6 +15,9 @@ public class BalancedParentheses {
 	}
 
 	public static boolean isFirstCharClosed(String string) {
-		return false;
+		if(CLOSE_LIST .contains(string.substring(0,1))) {
+			return false;
+		}
+		return true;
 	}
 }
