84be39bcab8c3198c964ca1d27a554610708d7a5
NaotoWada <naoto.cot25@gmail.com>
2019-05-29 22:54:45 +0900

    upgrade:三角測量により偶数判定を修正

--- src/mobpro/own/BalancedParentheses.java
+++ src/mobpro/own/BalancedParentheses.java
@@ -3,6 +3,9 @@ package mobpro.own;
 public class BalancedParentheses {
 
 	public static boolean isOdd(String string) {
+		if(string.length() % 2 ==0) {
+			return false;
+		}
 		return true;
 	}
 }
