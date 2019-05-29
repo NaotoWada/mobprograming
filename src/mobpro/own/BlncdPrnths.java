846a24b1210226a3cde9e27878007b2436f1f63c
NaotoWada <naoto.cot25@gmail.com>
2019-05-30 04:48:37 +0900

    clean:実行関数の定義位置移動と、ドキュメンテーションコメントの追加

--- src/mobpro/own/BlncdPrnths.java
+++ src/mobpro/own/BlncdPrnths.java
@@ -19,6 +19,38 @@ public class BlncdPrnths {
 		}
 	}
 
+	/**
+	 * 入力文字列のカッコがペアであり入れ子でない事を判定する実行関数.
+	 * <p>入力文字は次のダブルクォートで囲まれた6文字のみ許容する{@code "([{)]}" }
+	 * <p>許容された文字列以外が入力された場合動作を保証しない
+	 * @param string
+	 * 		入力文字列
+	 * @return 正常なペアの場合PARENTHESES.BALANCED、それ以外はPARENTHESES.UNBALANCED
+	 */
+	public static PARENTHESES judge(String string) {
+		if (isFirstClose(string) || isLastOpen(string)) {
+			// 最初と最後の要素が規定外の場合
+			return PARENTHESES.UNBALANCED;
+		}
+		if (isOdd(string)) {
+			// 奇数の場合は規定外
+			return PARENTHESES.UNBALANCED;
+		}
+
+		// スタックに詰めつつ判定を行う。1回でも入れ子になったら終了
+		Stack<String> stk = new Stack<>();
+		for (int i = 0; i < string.length(); i++) {
+			String elm = string.substring(i, i + 1);
+			boolean isContinue = isPairWhenEditingStack(stk, elm);
+			if (!isContinue) {
+				return PARENTHESES.UNBALANCED;
+			}
+		}
+
+		// すべての要素が入れ子でなかった
+		return PARENTHESES.BALANCED;
+	}
+
 	public static boolean isOdd(String parentheses) {
 		if (parentheses.length() % 2 == 0) {
 			return false;
@@ -41,6 +73,17 @@ public class BlncdPrnths {
 		return false;
 	}
 
+	/**
+	 * 入力されたスタックインスタンスを書き換えながら、ペア判定を行う関数
+	 * <p>スタックに格納されるのは”開きカッコ”のみ
+	 * <p>第二引数に閉じカッコを指定した場合、最後にスタックされたとのペアであればその要素をpopする
+	 * 
+	 * @param stck
+	 * 		スタックインスタンス。入力の参照は破壊的に変更される
+	 * @param prnths
+	 * 		スタックまたはpopするためのカッコ要素
+	 * @return true(開きカッコまたは閉じカッコで条件に一致した場合）/ false(それ以外)
+	 */
 	public static boolean isPairWhenEditingStack(Stack<String> stck, String prnths) {
 		if (OPEN_LIST.contains(prnths)) {
 			// 開きカッコの場合
@@ -71,29 +114,5 @@ public class BlncdPrnths {
 			throw new IllegalArgumentException("スタックされた要素に閉じカッコが入っていて不正です");
 		}
 		return PAIR.get(stackElement).equals(target);
-	}
-
-	public static PARENTHESES judge(String string) {
-		if (isFirstClose(string) || isLastOpen(string)) {
-			// 最初と最後の要素が規定外の場合
-			return PARENTHESES.UNBALANCED;
-		}
-		if (isOdd(string)) {
-			// 奇数の場合は規定外
-			return PARENTHESES.UNBALANCED;
-		}
-
-		// スタックに詰めつつ判定を行う。1回でも入れ子になったら終了
-		Stack<String> stk = new Stack<>();
-		for (int i = 0; i < string.length(); i++) {
-			String elm = string.substring(i, i + 1);
-			boolean isContinue = isPairWhenEditingStack(stk, elm);
-			if (!isContinue) {
-				return PARENTHESES.UNBALANCED;
-			}
-		}
-
-		// すべての要素が入れ子でなかった
-		return PARENTHESES.BALANCED;
 	}
 }
