a94d0f2ed63bd9d3f1f77922ba4b8f7b17b7d6c8
NaotoWada <naoto.cot25@gmail.com>
2019-05-29 23:11:12 +0900

    fix:メソッド名と期待結果が逆転していたので修正

--- src/mobpro/own/BalancedParenthesesTest.java
+++ src/mobpro/own/BalancedParenthesesTest.java
@@ -17,17 +17,17 @@ public class BalancedParenthesesTest {
 	}
 
 	@Test
-	public void test_最初が閉じカッコの場合はFlaseが返却される() {
-		assertFalse(BalancedParentheses.isFirstCharClosed("]"));
-		assertFalse(BalancedParentheses.isFirstCharClosed(")"));
-		assertFalse(BalancedParentheses.isFirstCharClosed("}"));
+	public void test_最初が閉じカッコの場合はTrueが返却される() {
+		assertTrue(BalancedParentheses.isFirstCharClosed("]"));
+		assertTrue(BalancedParentheses.isFirstCharClosed(")"));
+		assertTrue(BalancedParentheses.isFirstCharClosed("}"));
 
-		assertTrue(BalancedParentheses.isFirstCharClosed("["));
-		assertTrue(BalancedParentheses.isFirstCharClosed("("));
-		assertTrue(BalancedParentheses.isFirstCharClosed("{"));
+		assertTrue(BalancedParentheses.isFirstCharClosed("]["));
+		assertTrue(BalancedParentheses.isFirstCharClosed(")("));
+		assertTrue(BalancedParentheses.isFirstCharClosed("}{"));
 		
-		assertFalse(BalancedParentheses.isFirstCharClosed("]["));
-		assertFalse(BalancedParentheses.isFirstCharClosed(")("));
-		assertFalse(BalancedParentheses.isFirstCharClosed("}{"));
+		assertFalse(BalancedParentheses.isFirstCharClosed("["));
+		assertFalse(BalancedParentheses.isFirstCharClosed("("));
+		assertFalse(BalancedParentheses.isFirstCharClosed("{"));
 	}
 }
