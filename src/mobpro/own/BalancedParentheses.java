275d2509148493542f1dc752d3afac2c6cd0a202
NaotoWada <naoto.cot25@gmail.com>
2019-05-29 22:56:18 +0900

    clean:引数の値をリファクタリング

--- src/mobpro/own/BalancedParentheses.java
+++ src/mobpro/own/BalancedParentheses.java
@@ -2,8 +2,8 @@ package mobpro.own;
 
 public class BalancedParentheses {
 
-	public static boolean isOdd(String string) {
-		if(string.length() % 2 ==0) {
+	public static boolean isOdd(String parentheses) {
+		if(parentheses.length() % 2 ==0) {
 			return false;
 		}
 		return true;
