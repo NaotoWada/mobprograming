afa108806499c4adc9a345696c4eae2ab48b19ff
NaotoWada <naoto.cot25@gmail.com>
2019-05-29 23:24:59 +0900

    add:クラス名変更後のファイルコミット漏れ

---
+++ src/mobpro/own/BlncdPrnths.java
@@ -0,0 +1,32 @@
+package mobpro.own;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class BlncdPrnths {
+
+	private static final List<String> CLOSE_LIST = Arrays.asList("]", ")", "}");
+	private static final List<String> OPEN_LIST = Arrays.asList("[", "(", "{");
+
+	public static boolean isOdd(String parentheses) {
+		if (parentheses.length() % 2 == 0) {
+			return false;
+		}
+		return true;
+	}
+
+	public static boolean isFirstClose(String string) {
+		if (CLOSE_LIST.contains(string.substring(0, 1))) {
+			return true;
+		}
+		return false;
+	}
+
+	public static boolean isLastOpen(String string) {
+		int len = string.length();
+		if (OPEN_LIST.contains(string.substring(len-1, len))) {
+			return true;
+		}
+		return false;
+	}
+}
